70a8f19537bf331662fbf7a013bc2d9bb0be7082
VarunDhar <85066986+VarunDhar@users.noreply.github.com>
2023-01-05 17:03:06 +0530

    Update app.js
    Added new page "localhost:3000/work" showing activities related to work. 
    Constructed modules and exported them rather than reusing code for Date( ).

--- app.js
+++ app.js
@@ -2,32 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const app = express();
-
+const date = require(__dirname + "/getDate.js");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine","ejs");
 
 let items =["Wake up","Brush Teeth","Dress Up","Catch Bus"];
-
+let workItems = ["Check Emails","Make Schedule"];
 app.listen(3000,function(){
     console.log("Server is up and running at port 3000");
 });
 
 app.get('/', (req, res) => {
-    // ** alternate way to get current day **
-    // let currdate = new Date();
-    // let currday = currdate.getDay();
-    // let days= ["Sun","Mon","Tues","Wednes","Thurs","Fri","Satur"];
-    let options = {
-      weekday: 'long',
-      year: 'numeric', 
-      month: 'long', 
-      day: 'numeric'
-    };
 
-    let today = new Date();
-    let dayToday = today.toLocaleDateString("en-US", options);
-    res.render("list", {day: dayToday, tasks: items});
+    res.render("list", {day: date.getDate(), tasks: items});
 
   });
 app.post("/",function(req,res){
@@ -39,33 +27,19 @@ app.post("/",function(req,res){
 });
 
 
+app.get("/work", (req, res) => {
+  res.render("list", {day: "Work List", tasks: workItems});
+});
 
+app.post("/work",function(req,res){
 
+  let item = req.body.todo;
+  workItems.push(item);
+  res.redirect("/work");
 
-// const express = require("express");
-// const app = express();
-// const https = require("https");
-// const bodyParser = require("body-parser");
+});
 
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.set('view engine', 'ejs');
+app.get("/about",(req,res)=>{
+  res.render("about");
+});
 
-// var items = [];
-
-// app.listen("3000",function(){
-//   //
-// });
-
-// app.get("/",function(req,res){
-
-//   let date= new Date();
-//   let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-//   let today = date.toLocaleDateString("en-US", options);
-//   res.render("list",{currday : today.substring(0,today.search(",")),list : items});
-// });
-
-// app.post("/",function(req,res){
-//   var item = req.body.item;
-//   items.push(item);
-//   res.redirect("/");
-// });
